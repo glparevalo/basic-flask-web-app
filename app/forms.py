6e77a00c7ac6c8f7d96a764008f11e06f7f8b852
Gabriel Arevalo <143163497+glparevalo@users.noreply.github.com>
2025-05-19 17:48:22 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+# Main script to handle forms
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
